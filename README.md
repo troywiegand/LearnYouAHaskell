0443f99dea44459d152976f3895ac7d4b8db9452
Troy Wiegand <troy@troywiegand.com>
2019-12-04 14:47:51 -0500

    Finish List Comprehensions on Starting Out

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 I want to learn Haskell. 
 This is my journey.
+
+List Concat
+```
+[1,2] ++ [3,4]
+"hello" ++ " " ++ "world"
+```
+
+
--- baby.hs
+++ baby.hs
@@ -1,1 +1,17 @@
 doubleMe x = x+x
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+			then x
+			else 2*x
+
+doubleSmallNumberPlus1 x = (if x>100 then x else 2*x) +1
+
+conanO'Brien = "It's me, Conan O'Brien!" 
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+
+length' xs = sum [1| _ <- xs]
+
+onlyUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+onlyLowercase st = [c | c <- st, not (c `elem` ['A'..'Z'])]
